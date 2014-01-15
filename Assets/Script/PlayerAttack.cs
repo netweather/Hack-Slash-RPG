babdefcd4dd1971cb20b7865cfd6eaba64374273
netweather <qw2363101@163.com>
2014-01-16 07:06:10 +0800

    008.Targetting Enemies

---
+++ Assets/Script/PlayerAttack.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+	
+	public GameObject target;//游戏的TAG
+	public float attackTimer;//攻击时间
+	public float coolDown;	//冷却时间
+	
+	// Use this for initialization
+	void Start () {
+		attackTimer = 0;
+		coolDown = 2.0f;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(attackTimer >0)
+			attackTimer-=Time.deltaTime;
+		if(attackTimer <0)
+			attackTimer = 0;
+		
+	  if(Input.GetKeyUp(KeyCode.F)){
+			if(attackTimer ==0){
+				
+			Attack();
+				attackTimer = coolDown;
+				
+			}
+		}
+	}
+		private void Attack(){
+			float distance = Vector3.Distance(target.transform.position,transform.position);//定议距离
+				//距离    = 【三维向量】.距离【敌人的位置到Player.位置】
+			
+			Vector3 dir = (target.transform.position - transform.position).normalized;
+				//用它的位置和我们的位置创建一个向量.这个向量变为1个单位向量
+			float direction = Vector3.Dot (dir,transform.forward);
+				//方向    = 【三维向量】.点【dir向量*transform.前方的向量】
+				//如果等于1就在前方,如量等于-1则在相反方向,等于0则在侧面
+			Debug.Log(direction);
+		
+		if (distance <2.5f){
+			if (direction >0 ){
+			EnemyHealth eh=(EnemyHealth)target.GetComponent("EnemyHealth");//引用EnemyHealth脚本
+		
+		eh.AddjustCurrentHealth(-10)	;//引用EnemyHealth脚本的调整当前生命值
+			}
+		}
+	}
+}
---
+++ Assets/Script/Targetting.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Targetting : MonoBehaviour 
+{
+	public List<Transform> targets;//列表
+	public Transform selectedTarget;//选中的目标
+	private Transform myTransform;
+	
+	void Start () 
+	{
+		myTransform = transform;
+		targets = new List<Transform>();
+		selectedTarget = null;
+		AddAllEnemies();
+	}
+	/// <summary>
+	/// 添加气人敌人.
+	/// </summary>
+	public void AddAllEnemies()
+	{
+		GameObject[] go = GameObject.FindGameObjectsWithTag("Enemy");
+		
+		foreach(GameObject enemy in go) //for(元素类型t 元素变量x : 遍历对象obj)
+			AddTarget(enemy.transform);
+	}
+	
+	/// <summary>
+	/// 添加目标
+	/// </summary>
+	/// <param name='enemy'>
+	/// 敌人.
+	/// </param>
+	public void AddTarget(Transform enemy)
+	{
+		targets.Add (enemy);
+	}
+	
+	void Update () 
+	{
+		//按TAB键选择敌人
+		if(Input.GetKeyDown(KeyCode.Tab))
+		{
+			TargetEnemy();
+		}
+	}
+	/// <summary>
+	/// 选中敌人.
+	/// </summary>
+	private void TargetEnemy()
+	{
+		if(selectedTarget == null)
+		{
+			SortTargetByDistance();// 根据距离排列目标.
+			selectedTarget = targets[0];//选择最近的
+		}
+		else
+		{
+			int index = targets.IndexOf(selectedTarget);
+			//索引     =目标.    是索引(选中的目标)
+		
+			if(index < targets.Count - 1)
+		   //如果(索引小于目标.数量    -1)		
+		//ps:选中的物体是否小于目标列表所包含的元素个娄 -1 也就是比我们所有元素个数的数量少1
+			{
+				index++;
+			}
+			else
+			{
+				index = 0;
+			}
+			DeselectTarget();
+			selectedTarget = targets[index];
+			//选中的目标    =目标[索引]
+		}
+		SelectTarget();//选中的目标
+	}
+	/// <summary>
+	/// 根据距离排列目标.
+	/// </summary>
+	private void SortTargetByDistance()
+	{
+		targets.Sort (delegate(Transform t1,Transform t2)
+		//目标.排列 (委托	
+		{
+			return Vector3.Distance(t1.position,myTransform.position).CompareTo(Vector3.Distance(t2.position, myTransform.position));
+			//返回  三维向量.距离 比较(t1.位置    和我们自己的位置)      .比较       (三维向量.距离    (t2.距离     和我我们的距离)
+		});
+	}
+	/// <summary>
+	/// 选择目标.
+	/// </summary>
+	private void SelectTarget()
+	{
+		selectedTarget.renderer.material.color = Color.yellow;
+		//选中的目标.渲染器.材质.颜色
+		PlayerAttack pa = (PlayerAttack)GetComponent("PlayerAttack");
+		//					(玩家攻击)获取成分
+		pa.target = selectedTarget.gameObject;
+	}
+	/// <summary>
+	/// 取消选定目标.
+	/// </summary>
+	private void DeselectTarget()
+	{
+		selectedTarget.renderer.material.color = Color.white;
+		//选中的目标.渲染器.材质.颜色
+		selectedTarget = null;
+	}
+}
